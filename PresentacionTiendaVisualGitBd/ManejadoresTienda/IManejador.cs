67f7fa801b704fae042d3ed653c36d3d6966a2eb
Oswaldo Rafael Ortega Garcia <oswaldoortega680@gmail.com>
2022-09-16 16:43:26 -0500

    Se hizo el manejador para jugar los productos

---
+++ PresentacionTiendaVisualGitBd/ManejadoresTienda/IManejador.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManejadoresTienda
+{
+    interface IManejador
+    {
+        void Guardar(dynamic Entidad);
+        void Borrar(dynamic Entidad);
+        void Mostrar(DataGridView tabla, string filtro);
+    }
+}
